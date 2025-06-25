ce6bfab95824c43f021db2c618d58825937cc192
damianvu <minhvdq3008@gmail.com>
2025-06-25 16:00:12 -0400

    user service update

--- backend/service/userService.js
+++ backend/service/userService.js
@@ -6,19 +6,23 @@ const getAllUsers = async () => {
 }
 
 const getUserById = async () => {
-
+    const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [id])
+    return rows[0]
 }
 
 const createUser = async () => {
-
+    const [result] = await db.query('INSERT INTO users (first_name, last_name, email, hash_password, is_admin) VALUES (?, ?, ?, ?, ?)', [firstName, lastName, email, hashPassword, isAdmin])
+    return result
 }
 
-const getUserByEmail = async () => {
-
+const getUserByEmail = async (email) => {
+    const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email])
+    return rows[0]
 }
 
-const deleteUser = async () => {
-    
+const deleteUser = async (id) => {
+    const [result] = await db.query('DELETE FROM users WHERE id = ?', [id])
+    return result
 }
 
 module.exports = {
