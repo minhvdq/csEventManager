6b0b09706cd21f663302c57abcfe9562bb2f41a6
Damian_may_truong <vudimi01@gettysburg.edu>
2025-07-03 11:25:24 -0400

    verify email

--- backend/service/authService.js
+++ backend/service/authService.js
@@ -1,7 +1,15 @@
 const db = require('../utils/db')
 const crypto = require('crypto')
 const bcrypt = require('bcrypt')
+const {backendBase} = require('../utils/homeUrl')
 
 const requestVerifyEmail = async (email) => { 
-    let token = await db.query(`SELECT * FROM `)
+    await db.query(`DELETE FROM Token WHERE email = ?`, [email])
+
+    const verifyToken = crypto.randomBytes(32).toString('hex')
+    const hashedToken = await bcrypt.hash(verifyToken, 10)
+
+    const savedTokenResponse = await db.query(`INSERT INTO Token (email, token) VALUES (?, ?)`, [email, hashedToken])
+
+    const link = `${}`
 }
---
+++ backend/utils/homeUrl.js
@@ -0,0 +1,4 @@
+const backendBase = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://rate-my-classes-gb.fly.dev'
+const frontendBase = process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://rate-my-classes-gb.fly.dev'
+
+module.exports = {backendBase, frontendBase}
