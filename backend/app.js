d94d9b613e7951d96faaf029b850c51d50dcd2ed
Damian@Vu <minhvdq@gmail.com>
2025-08-22 22:52:38 +1000

    add https to cors

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://138.234.44.100',
     'http://cs100.cc.gettysburg.edu',
-    'http://acm.gettysburg.edu'
+    'http://acm.gettysburg.edu',
+    'https://acm.gettysburg.edu'
 ];
 
 app.use((req, res, next) => {
